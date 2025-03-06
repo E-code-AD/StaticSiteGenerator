9fd9e194469812906e6cd3b0309bb6aca7c8d302
E-code-AD <drzewieckiandrzejkonrad@gmail.com>
2025-03-06 13:37:17 +0100

    final generator added

--- src/main.py
+++ src/main.py
@@ -3,16 +3,33 @@
 from copystatic import copy_files_recursive
 import os
 import shutil
-
+from generate_page import generate_page,generate_pages_recursive
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
+    generate_pages_recursive(dir_path_content,template_path,dir_path_static)
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    # pages = {"index.md":"index.html",
+    #     "blog/glorfindel/index.md":"blog/glorfindel/index.html",
+    #     "blog/tom/index.md":"blog/tom/index.html",
+    #     "blog/majesty/index.md":"blog/majesty/index.html",
+    #     "contact/index.md":"contact/index.html"}
+    # for page in pages:
+    #     generate_page(
+    #         os.path.join(dir_path_content, page),
+    #         template_path,
+    #         os.path.join(dir_path_public, pages[page]),
+    #     )
+    
 
 main()
